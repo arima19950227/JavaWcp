331c095de686a20c73065340a5c2316985611680
arimadaichi <arimadaichi@localhost>
2023-01-23 23:31:20 +0900

    first commit

---
+++ src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int number = 9;
+		String str = "有馬大地";
+	    System.out.println(str);
+		str="中島さん";
+		System.out.println(str);
+		System.out.println(number);
+	}
+}
